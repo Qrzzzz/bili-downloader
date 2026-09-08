e23992a3b22304476bd0dba01cf148c7af39234a
Cherry Chu <qrzzzz1412@gmail.com>
2026-09-08 10:21:43 +0800

    fix: validate media and preserve download results for v2.10 (#27)

--- BiliDownloader.WinUI/App.xaml.cs
+++ BiliDownloader.WinUI/App.xaml.cs
@@ -5,7 +5,7 @@ namespace BiliDownloader.WinUI;
 
 public partial class App : Application
 {
-    public const string AppVersion = "2.9";
+    public const string AppVersion = "2.10";
     public static ApplicationSession Session { get; private set; } = null!;
     public static MainWindow MainWindow { get; private set; } = null!;
     public App()
