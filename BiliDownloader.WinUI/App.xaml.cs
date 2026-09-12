6381b24f7a49d210543f9621474828d6249b5a8b
Qrzzzz <qrzzzz1412@gmail.com>
2026-09-12 12:22:26 +0800

    fix: settle worker startup failures and deferred cancellation for v2.11

--- BiliDownloader.WinUI/App.xaml.cs
+++ BiliDownloader.WinUI/App.xaml.cs
@@ -5,7 +5,7 @@ namespace BiliDownloader.WinUI;
 
 public partial class App : Application
 {
-    public const string AppVersion = "2.10";
+    public const string AppVersion = "2.11";
     public static ApplicationSession Session { get; private set; } = null!;
     public static MainWindow MainWindow { get; private set; } = null!;
     public App()
